f3fa016a28dd722795aa77b23d4fa1fc51addf6e
Gerardo Steven Quintanilla Lopez <gerardo@MacBook-Pro-de-Gerardo.local>
2026-04-23 18:25:01 -0600

    Reportes 1 al 4

--- Reportes_Gerardo.py
+++ Reportes_Gerardo.py
@@ -12,29 +12,25 @@ print("=" * 45)
 print("Reporte 1 - Cantidad de personas por ciudad")
 print("=" * 45)
 
-# Contar personas por ciudad 
 ciudades = {}
 
 # Se recorre el diccionario para obtener agregar la ciudad o aumentarle la cantidad 
 # si ya esta agregado al diccionario
-
 for persona in personas:
     ciudad = persona["ciudad"]
-    if ciudad in ciudades:
-        ciudades[ciudad] += 1
-    else: 
-        ciudades[ciudad] = 1
+    if ciudad not in ciudades:
+        ciudades[ciudad] = {"cantidad": 0}
+    ciudades[ciudad]["cantidad"] += 1
 
 # Se ordenan las ciudades de mayor a menor para tener mas orden 
-ciudades_ordenadas = sorted(ciudades.items(), key=lambda x: x[1], reverse=True)
+ciudades_ordenadas = sorted(ciudades.items(), key=lambda x: x[1]["cantidad"], reverse=True)
 
-# Mostrar resultados
 print(f"{'Ciudad': <20} {'Cantidad': >10} {'Porcentaje':>12}")
 print("-" * 45)
 # Se recorre el diccionario para obtener las ciudades y la cantidad total y obtener el porcentaje
-for ciudad, cantidad in ciudades_ordenadas:
-    porcentaje = (cantidad / cantidad_personas) * 100
-    print(f"{ciudad:<20} {cantidad:>10} {porcentaje:>11.2f}%")
+for ciudad, datos in ciudades_ordenadas:
+    porcentaje = (datos["cantidad"] / cantidad_personas) * 100
+    print(f"{ciudad:<20} {datos['cantidad']:>10} {porcentaje:>11.2f}%")
 
 print("-" * 45)
 print(f"{'TOTAL':<20} {cantidad_personas:>10} {'100.00%':>12}")
@@ -44,29 +40,25 @@ print("=" * 45)
 print("Reporte 2 - Cantidad de personas por carrera")
 print("=" * 45)
 
-# Contar personas por carrera
 carreras = {}
 
 # Se recorre el diccionario para obtener agregar la ciudad o aumentarle la carrera 
 # si ya esta agregado al diccionario
-
 for persona in personas:
     carrera = persona["carrera"]
-    if carrera in carreras:
-        carreras[carrera] += 1
-    else: 
-        carreras[carrera] = 1
+    if carrera not in carreras:
+        carreras[carrera] = {"cantidad": 0}
+    carreras[carrera]["cantidad"] += 1
 
 # Se ordenan las carreras de mayor a menor para tener mas orden 
-carreras_ordenadas = sorted(carreras.items(), key=lambda x: x[1], reverse=True)
+carreras_ordenadas = sorted(carreras.items(), key=lambda x: x[1]["cantidad"], reverse=True)
 
-# Mostrar resultados
 print(f"{'Carrera': <20} {'Cantidad': >10} {'Porcentaje':>12}")
 print("-" * 45)
 # Se recorre el diccionario para obtener las ciudades y la cantidad total y obtener el porcentaje
-for carrera, cantidad in carreras_ordenadas:
-    porcentaje = (cantidad / cantidad_personas) * 100
-    print(f"{carrera:<20} {cantidad:>10} {porcentaje:>11.2f}%")
+for carrera, datos in carreras_ordenadas:
+    porcentaje = (datos["cantidad"] / cantidad_personas) * 100
+    print(f"{carrera:<20} {datos['cantidad']:>10} {porcentaje:>11.2f}%")
 
 print("-" * 45)
 print(f"{'TOTAL':<20} {cantidad_personas:>10} {'100.00%':>12}")
@@ -76,7 +68,6 @@ print("=" * 45)
 print("Reporte 3 - Promedio General Académico")
 print("=" * 45)
 
-# Diccionario anidado para el cálculo
 reporte_promedio = {
     "suma": 0.0,
     "conteo": 0,
@@ -84,16 +75,13 @@ reporte_promedio = {
 }
 
 for persona in personas:
-    # Validación para asegurar que la clave existe
     if "promedio" in persona and persona["promedio"].strip() != "":
         reporte_promedio["suma"] += float(persona["promedio"])
         reporte_promedio["conteo"] += 1
 
-# Calcular promedio solo si hay registros válidos
 if reporte_promedio["conteo"] > 0:
     reporte_promedio["promedio_final"] = reporte_promedio["suma"] / reporte_promedio["conteo"]
 
-# Mostrar resultados usando .items()
 for clave, valor in reporte_promedio.items():
     if clave == "promedio_final":
         print(f"El promedio general es: {round(valor, 2)}")
@@ -106,13 +94,11 @@ print("=" * 45)
 print("Reporte 4 - Promedio Académico por Carrera")
 print("=" * 45)
 
-# Contar personas por carrera 
 carreras = {}
 
 for persona in personas:
     carrera = persona["carrera"]
     
-    # Validación
     if carrera not in carreras:
         carreras[carrera] = {
             "suma": 0.0,
@@ -120,20 +106,16 @@ for persona in personas:
             "promedio": 0.0
         }
     
-    # Validar que el promedio no esté vacío
     if "promedio" in persona and persona["promedio"].strip() != "":
         carreras[carrera]["suma"] += float(persona["promedio"])
         carreras[carrera]["conteo"] += 1
 
-# Calcular promedio por carrera
 for carrera, datos in carreras.items():
     if datos["conteo"] > 0:
         datos["promedio"] = datos["suma"] / datos["conteo"]
 
-# Ordenar por promedio de mayor a menor
 carreras_ordenadas = sorted(carreras.items(), key=lambda x: x[1]["promedio"], reverse=True)
 
-# Mostrar resultados
 print(f"{'Carrera':<25} {'Estudiantes':>12} {'Promedio':>10}")
 print("-" * 50)
 
